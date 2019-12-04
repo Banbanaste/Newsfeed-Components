8d38bfe765041a16efde2fd58ed671d11c4f418a
Roberto Banbanaste <rbanbanaste@outlook.com>
2019-12-04 17:42:23 -0500

    completed assignment

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,12 @@
 /* This is the data we will be using, study it but don't change anything, yet. */
 
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out"
 ];
 
 /* 
@@ -33,3 +33,25 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+
+function createMenu(arr) {
+  const menu = document.createElement("div");
+  const ul = document.createElement("ul");
+
+  arr.forEach(item => {
+    let el = document.createElement("li");
+    el.textContent = item;
+    ul.appendChild(el);
+  });
+
+  menu.classList.add("menu");
+  menu.appendChild(ul);
+
+  document.querySelector(".menu-button").addEventListener("click", event => {
+    menu.classList.toggle("menu--open");
+  });
+
+  return menu;
+}
+const header = document.querySelector(".header");
+header.append(createMenu(menuItems));
